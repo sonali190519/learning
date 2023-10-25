aab7e2f099428b5ce6a9201cc3f3859a1571732e
sonali190519 <sonali260115@gmail.com>
2023-10-24 21:03:21 -0700

    added new file

---
+++ Sample1 - Copy.py
@@ -0,0 +1,9 @@
+name="Sonali"
+print("Hello Everyone!! " + name)
+superhero=("Sonali","Digambar","Saee","Sameer","Monish","Nitya",True,44)
+ele_Superhero=len(superhero)
+print(ele_Superhero)
+
+print(superhero)
+print(superhero[2])
+print(f'Verify the item in the list or not :{"Saee"in superhero}')
